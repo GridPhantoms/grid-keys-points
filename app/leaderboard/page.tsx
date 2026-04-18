d498b5b0b3f128639c09956c1b88fd5ba9d1b0a5
Kirby Trapolino <kirbytrapolino@Mac.attlocal.net>
2026-04-17 20:23:30 -0500

    Update Keyholder Leaderboard snapshot time

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -12,7 +12,7 @@ export default function Leaderboard() {
   const [loadingBytes, setLoadingBytes] = useState(true);
   const [loadingKeyholders, setLoadingKeyholders] = useState(false);
   const [errorKeyholders, setErrorKeyholders] = useState('');
-  const [lastSnapshot] = useState("April 17, 2026 01:18 UTC");
+  const [lastSnapshot] = useState("April 18, 2026 01:18 UTC");
   const [menuOpen, setMenuOpen] = useState(false);
   const [copiedWallet, setCopiedWallet] = useState<string | null>(null);
 

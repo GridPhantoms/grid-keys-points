2639981fb621b0f64c0215aff267fd1f2d12a451
GridPhantoms <274114385+GridPhantoms@users.noreply.github.com>
2026-05-27 09:35:20 -0500

    Vault snapshot: DeBank $40,176.00 (2026-05-27)

--- app/engine/page.tsx
+++ app/engine/page.tsx
@@ -77,7 +77,7 @@ export default function EngineRoom() {
   const TOTAL_EXODUS_SUPPLY = 3333;
 
   const GENESIS_LAUNCH = new Date('2025-10-09T16:03:47Z').getTime();
-  const LAST_SNAPSHOT = "May 25, 2026 15:00 UTC";
+  const LAST_SNAPSHOT = "May 27, 2026 14:35 UTC";
 
   useEffect(() => {
     const loadData = async () => {

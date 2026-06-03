f79ea8255eb05fc97cd4b1d1587bd5eff497b19a
GridPhantoms <ktrap480@gmail.com>
2026-06-03 08:14:21 -0500

    Update vault snapshot DeBank total

--- app/engine/page.tsx
+++ app/engine/page.tsx
@@ -77,7 +77,7 @@ export default function EngineRoom() {
   const TOTAL_EXODUS_SUPPLY = 3333;
 
   const GENESIS_LAUNCH = new Date('2025-10-09T16:03:47Z').getTime();
-  const LAST_SNAPSHOT = "June 1, 2026 12:48 UTC";
+  const LAST_SNAPSHOT = "June 3, 2026 13:13 UTC";
 
   useEffect(() => {
     const loadData = async () => {

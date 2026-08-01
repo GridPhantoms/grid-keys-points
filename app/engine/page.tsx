dd70cdfe53127b1b1605038105477f7dedc59577
GridPhantoms <ktrap480@gmail.com>
2026-08-01 07:08:25 -0500

    Update vault snapshot

--- app/engine/page.tsx
+++ app/engine/page.tsx
@@ -108,7 +108,7 @@ export default function EngineRoom() {
   const TOTAL_EXODUS_SUPPLY = 3333;
 
   const GENESIS_LAUNCH = new Date('2025-10-09T16:03:47Z').getTime();
-  const LAST_SNAPSHOT = "July 31, 2026 14:35 UTC";
+  const LAST_SNAPSHOT = "August 1, 2026 12:08 UTC";
   const [currentTime] = useState(() => Date.now());
 
   useEffect(() => {
